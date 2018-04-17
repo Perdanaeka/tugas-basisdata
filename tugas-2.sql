8e6c4a04e7f80ed1661ffd73c14cd7eac2c54a65
Perdanaeka <perdanaeka99@gmail.com>
2018-04-17 15:42:52 +0100

    Update and rename tugas-2 to tugas-2.sql

--- tugas-2.sql
+++ tugas-2.sql
@@ -4,19 +4,18 @@ CREATE TABLE dosen (nip char(10), nama varchar(50),alamat varchar(150), Agama va
 CREATE TABLE matakuliah (kode char(10), nama varchar(50),sks int (11));
 CREATE TABLE krs (nim char(10), kode char(10),nip char(10),
 
-// masukkan data dosen
+--masukkan data dosen
 
 INSERT INTO dosen (nip,nama,alamat,agama)VALUES(‘B001’,’saka’,’SURABAYA’,’islam’);
 INSERT INTO dosen (nip,nama,alamat,agama)VALUES(‘B002’,’loli’,’YOGYAKARTA’,’islam’);
 INSERT INTO dosen (nip,nama,alamat,agama)VALUES(‘B003’,’waisa’,’JAKARTA’,’islam’);
-
-// data mahasiswa
+--data mahasiswa
 
 INSERT INTO mahasiswa (nim,nama,alamat,agama)VALUES(‘1701’,’neko’,’jl.almahera’,’islam’);
 INSERT INTO mahasiswa (nim,nama,alamat,agama)VALUES(‘1702’,’miko’,’jl.melati’,’kristen’);
 INSERT INTO mahasiswa (nim,nama,alamat,agama)VALUES(‘1703’,’kiko’,’jl.mawar’,’islam’);
 
-// data krs
+--data krs
 
 INSERT INTO matakuliah (kode,nama,sks)VALUES(‘aaa’,’Kalkulus’,’4’);
 INSERT INTO matakuliah (kode,nama,sks)VALUES(‘bbb’,’Matematika’,’4’);
