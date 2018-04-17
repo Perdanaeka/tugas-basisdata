d2aa228695cc676fe352b4a35c2fd58aacd4479c
Perdanaeka <perdanaeka99@gmail.com>
2018-04-17 15:42:12 +0100

    Update tugas-3.sql

--- tugas-3.sql
+++ tugas-3.sql
@@ -1,14 +1,14 @@
-//menentukan nilai akhir
+--menentukan nilai akhir
    update krs set akhir  = ‘(40/100*uts)+(60/100*uas)’;
 
-//update untuk menentukan huruf
+--update untuk menentukan huruf
    update krs set huruf
    update krs set huruf = “A” where akhir >=85;
    update krs set huruf = “B” where (akhir < 85)&&(akhir >=75);
    update krs set huruf = “C” where (akhir < 75)&&(akhir >=60);
    update krs set huruf = “D” where (akhir < 60)&&(akhir >=40);
    update krs set huruf = “E” where akhir < 40;
-//update untuk menentukan bobot
+--update untuk menentukan bobot
    update krs set bobot = “4” where huruf = “A”;
    update krs set bobot = “3” where huruf = “B”;
    update krs set bobot = “2” where huruf = “C”;
