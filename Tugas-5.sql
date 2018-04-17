12e735bd22d61f348b6215b6d105e22bd86b5407
Perdanaeka <perdanaeka99@gmail.com>
2018-04-17 15:56:42 +0100

    Create Tugas-5.sql

---
+++ Tugas-5.sql
@@ -0,0 +1,9 @@
+select mahasiswa.nim, krs.huruf from mahasiswa where krs.huruf='B' ;
+
+select disticnt nim,huruf,kode from krs where kodemat>=2 in (huruf='B');
+
+select avg(uts) as rata_rata from krs group by kode;
+
+select avg(krs.akhir) as rata_rata from matakuliah, krs, dosen group by dosen.nip;
+
+select mahasiswa.nama , avg(krs.akhir) as rata_rata from mahasiswa, krs having max(rata_rata);
